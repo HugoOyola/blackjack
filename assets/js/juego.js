296d2e054e37bad4b2e54bd4c52a0b3d4f019c94
Hugo Oyola <hugo.ham@outlook.com>
2026-08-05 00:56:54 -0500

    feat: Crear Baraja de cartas.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,14 +1,30 @@
 /**
- * 2C - Dos de Corazones
- * 2D - Dos de Diamantes
- * 2H - Dos de Tréboles
- * 2S - Dos de Picas
+ * 2C - Two de Clubs
+ * 2D - Two de Diamonds
+ * 2H - Two de Heart
+ * 2S - Two de Spades
  * **/
 
 let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
 
 const crearDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
+    }
+  }
 
+  for (let tipo of tipos) {
+    for (let esp of especiales) {
+      deck.push(esp + tipo);
+    }
+  }
+
+  console.log(deck);
+  deck = _.shuffle(deck);
+  console.log(deck);
 }
 
 crearDeck();
